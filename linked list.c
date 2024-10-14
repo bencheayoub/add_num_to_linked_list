dd7c9317d8e1b335eb4ca6efc50a0924352b9586
bencheayoub <147947828+bencheayoub@users.noreply.github.com>
2024-10-14 21:52:22 +0100

    Add files via upload

---
+++ linked list.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include <stdlib.h> 
+
+typedef struct node
+{
+   int data;
+   struct node *next;
+}Node;
+Node addnum(Node *head, int num)
+{
+    Node *temp = head;
+    Node *foot;
+
+    while(foot != NULL){
+        foot = foot->next;
+        }
+    if (temp->data + num > 9)
+    {
+        while(temp != foot)
+        {
+            temp = temp->next;
+        }
+        addnum(Node *temp, 1);
+        temp = head;
+    }
+    else{
+        foot->data += num;
+        while(temp != foot)
+        {
+            temp = temp->next;
+        }
+        foot = temp;
+    }
+    return head;
+}
